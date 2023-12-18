00302c3561c3ea813d468686da473c5116edca96
23MH1A05O2 <147487947+23MH1A05O2@users.noreply.github.com>
2023-12-18 15:31:34 +0530

    #include<stdio.h> int main() {     int n,sum=0;     scanf("%d",&n);     int a[n],i;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     int min=a[0];     for(i=0;i<n;i++)     {         if(min>a[i])         {             min=a[i];         }     }     printf("%d",min); }

---
+++ Minimum_element_of_an_array.c
@@ -0,0 +1,21 @@
+
+#include<stdio.h>
+int main()
+{
+    int n,sum=0;
+    scanf("%d",&n);
+    int a[n],i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int min=a[0];
+    for(i=0;i<n;i++)
+    {
+        if(min>a[i])
+        {
+            min=a[i];
+        }
+    }
+    printf("%d",min);
+}
