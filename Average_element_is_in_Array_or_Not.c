68bec7edd0f916eb5b141bcc9fef93c8b02ecc82
23MH1A05O2 <147487947+23MH1A05O2@users.noreply.github.com>
2023-12-19 13:50:40 +0530

    #include<stdio.h> int main() {     int n,avg,sum=0,flag=0;     scanf("%d",&n);     int a[n],i;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         sum+=a[i];     }     avg=sum/n;     for(i=0;i<n;i++)     {         if(avg==a[i])         {             flag=1;             break;         }     }     if(flag==1)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Average_element_is_in_Array_or_Not.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int n,avg,sum=0,flag=0;
+    scanf("%d",&n);
+    int a[n],i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        sum+=a[i];
+    }
+    avg=sum/n;
+    for(i=0;i<n;i++)
+    {
+        if(avg==a[i])
+        {
+            flag=1;
+            break;
+        }
+    }
+    if(flag==1)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
